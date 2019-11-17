69e5a0610e21808d1574f837044be51ffd825ff7
Sparkenstein <zetabytes.pp@gmail.com>
2019-11-17 23:04:44 +0530

    [feature] - reading config from file, tmp solution

--- src/internals/deploy.rs
+++ src/internals/deploy.rs
@@ -1,5 +1,7 @@
 use clap::ArgMatches;
 
 pub fn init(value: &ArgMatches){
-    println!("Deploying server with name {:?}", value.value_of("servername").unwrap());
+    // println!("Deploying server with name {:?}", value.args);
+    let config: &str = include!("files/nginx.conf.sample");
+    println!("{}", config.replace("${file}", "/etc/file"))
 }
