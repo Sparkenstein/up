d84b6522adf8a218244100cb3ac0623330a3bd69
Sparkenstein <zetabytes.pp@gmail.com>
2019-11-17 21:40:05 +0530

    [feature] - matching on subcommand

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,18 @@ extern crate clap;
 use clap::{App, Arg, SubCommand};
 
 fn main() {
-    let matches = App::new("up")
+    let args = App::new("up")
         .version("1.0")
         .author("Sparkenstein")
         .about("nginx management made easy!")
         .subcommand(
             SubCommand::with_name("deploy")
-                .about("generates all the files automatically and deploys them")
-                .arg(Arg::with_name("servername").takes_value(true)),
+                .about("generates all the files automatically and deploys the server")
+                .arg(Arg::with_name("servername").required(true)),
         )
         .get_matches();
-    println!("{:?}", matches)
+    match args.subcommand_name() {
+        Some("deploy") => println!("Deploying new server"),
+        _ => unreachable!()
+    }
 }
