0ca524aaeeb009a2ee66c9b42724dbabcff6e66a
Sparkenstein <zetabytes.pp@gmail.com>
2020-03-10 16:26:45 +0530

    updated params, new default config

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,29 @@ fn main() {
         .subcommand(
             SubCommand::with_name("deploy")
                 .about("generates all the files automatically and deploys the server")
-                .arg(Arg::with_name("servername").required(true)),
+                .arg(
+                    Arg::with_name("servername")
+                        .required(true)
+                        .help("Name of the server. it will be used for `server` property in nginx"),
+                )
+                .arg(
+                    Arg::with_name("port")
+                        .default_value("80")
+                        .short("p")
+                        .long("port")
+                        .help("Port at which this server listens"),
+                )
+                .arg(
+                    Arg::with_name("webroot")
+                        .default_value("/var/www/<servername>")
+                        .short("w")
+                        .long("webroot")
+                        .help("root of the server."),
+                ),
         )
         .get_matches();
     match args.subcommand() {
         ("deploy", Some(sub)) => internals::deploy::init(sub),
-        _ => unreachable!()
+        _ => eprintln!("Please provide a command. use -h for help"),
     }
 }
