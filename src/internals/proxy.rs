0d69fcde4f9772d66f259d7e6093a767231238bd
Spark <a>
2020-07-06 01:38:22 +0530

    [refactor] moved common conf generation to different subcommand

--- src/internals/proxy.rs
+++ src/internals/proxy.rs
@@ -6,14 +6,13 @@ pub fn init(deploy_args: &ArgMatches) {
     let servername = deploy_args.value_of("servername").unwrap();
     let inport = deploy_args.value_of("inport").unwrap();
     let outport = deploy_args.value_of("outport").unwrap();
-    let mut nginx_path = PathBuf::new();
 
     let config = include!("files/proxy_server.conf.sample")
         .replace("{{inport}}", inport)
         .replace("{{outport}}", outport)
         .replace("{{domain}}", servername);
 
-    nginx_path.push(format!("/etc/nginx/sites-available/{}.conf", servername));
+    let nginx_path = PathBuf::from(format!("/etc/nginx/sites-available/{}.conf", servername));
 
     utils::backup_config();
 
--- src/internals/statics.rs
+++ src/internals/statics.rs
@@ -5,7 +5,6 @@ use std::path::PathBuf;
 pub fn init(deploy_args: &ArgMatches) {
     let servername = deploy_args.value_of("servername").unwrap();
     let port = deploy_args.value_of("port").unwrap();
-    let mut nginx_path = PathBuf::new();
 
     let webroot;
     if deploy_args.occurrences_of("webroot") == 0 {
@@ -17,13 +16,15 @@ pub fn init(deploy_args: &ArgMatches) {
         webroot = deploy_args.value_of("webroot").unwrap().to_string();
     }
 
-    let config = include!("files/server.conf.sample")
+    let server_config = include!("files/server.conf.sample")
         .replace("{{port}}", port)
         .replace("{{webroot}}", &webroot)
         .replace("{{domain}}", servername);
 
+    let nginx_path = PathBuf::from(format!("/etc/nginx/sites-available/{}.conf", servername));
+
     // creating file later to avoid orphan files creating incase above code breaks.
-    nginx_path.push(format!("/etc/nginx/sites-available/{}.conf", servername));
+    // nginx_path.push();
     if nginx_path.exists() {
         eprintln!(
             "Site with {} name already exists at sites-available/",
@@ -31,11 +32,8 @@ pub fn init(deploy_args: &ArgMatches) {
         );
         std::process::exit(1);
     }
-    utils::backup_config();
 
-    utils::gen_dh_params();
-
-    utils::write_file(&nginx_path, config);
+    utils::write_file(&nginx_path, server_config);
 
     utils::make_symlink(nginx_path);
 }
